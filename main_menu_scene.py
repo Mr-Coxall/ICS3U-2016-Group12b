54f632b5e74b00ba745e9833d9742486de1f8b97
Patrick Coxall <mr.coxall@mths.ca>
2017-01-25 21:54:26 -0500

    Create main_menu_scene.py

---
+++ main_menu_scene.py
@@ -0,0 +1,57 @@
+# Created by: Mr. Coxall
+# Created on: Sep 2016
+# Created for: ICS3U
+# This scene shows the main menu.
+
+from scene import *
+
+from main_menu_scene import *
+
+
+class MainMenuScene(Scene):
+    def setup(self):
+        # this method is called, when user moves to this scene
+        
+        #create size of screen variables
+        self.size_of_screen_x = self.size.x
+        self.size_of_screen_y = self.size.y
+        self.center_of_screen_x = self.size_of_screen_x/2
+        self.center_of_screen_y = self.size_of_screen_y/2
+        
+        # add MT blue background color
+        background_position = Vector2(self.center_of_screen_x, self.center_of_screen_y)
+        self.background = SpriteNode(position = background_position, 
+                                     color = 'white', 
+                                     parent = self, 
+                                     size = self.size)
+    
+    def update(self):
+        # this method is called, hopefully, 60 times a second
+        pass
+    
+    def touch_began(self, touch):
+        # this method is called, when user touches the screen
+        pass
+    
+    def touch_moved(self, touch):
+        # this method is called, when user moves a finger around on the screen
+        pass
+    
+    def touch_ended(self, touch):
+        # this method is called, when user releases a finger from the screen
+        pass
+    
+    def did_change_size(self):
+        # this method is called, when user changes the orientation of the screen
+        # thus changing the size of each dimension
+        pass
+    
+    def pause(self):
+        # this method is called, when user touches the home button
+        # save anything before app is put to background
+        pass
+    
+    def resume(self):
+        # this method is called, when user place app from background 
+        # back into use. Reload anything you might need.
+        pass
